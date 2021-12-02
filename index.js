e5b0e992ec453933e96502505b7164e5f31709d3
Trefu_ <tre.lopez@hotmail.com>
2021-12-02 19:09:28 -0300

    limpio codigo

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
 //MODULOS NECESARIOS
 const fs = require("fs");
-const rpgDiceRoller = require("rpg-dice-roller");
 const qrcode = require("qrcode-terminal");
-const SESSION_FILE_PATH = "./session.json";
-//Mis funciones
-const { prefixeCall } = require("./config.json");
-const { playersNames, printPcStats, modifier } = require("./utils/utils");
-const commandHandler = require('./commands/commandHandler')
-var contacts;
-let sessionCfg;
-var players;
-
-
-//para iniciar sesion con otro dispositivo borrar el archivo sessions.json
-if (fs.existsSync(SESSION_FILE_PATH)) {
-  sessionCfg = require(SESSION_FILE_PATH);
-}
-
 const { Client } = require("whatsapp-web.js");
-const { MessageMedia } = require("whatsapp-web.js");
-
+const commandHandler = require('./commands/commandHandler')
+const SESSION_FILE_PATH = "./session.json";
 
 require('./database')
-// wsp web api pedrolopez github
 
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
 const client = new Client({
-  puppeteer: {
-    headless: false,
-  },
-  session: sessionCfg
+  session: sessionData
 });
 
-client.initialize();
+client.on('qr', qr => {
+  qrcode.generate(qr, {small: true});
+});
 
-client.on("authenticated", (session) => {
-  sessionCfg = session;
-  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-    if (err) {
-      console.error(err);
-    }
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+      if (err) {
+          console.error(err);
+      }
   });
 });
 
@@ -49,11 +39,9 @@ client.on("auth_failure", (msg) => {
 
 client.on("ready", async () => {
   console.log("Client is ready!");
-  /*   contacts = await client.getContacts();
-    players = await Personajes.find();
-    let test = await playersNames(); */
 });
 
+client.initialize();
 
 //al recibir un mensaje se lo pasa directamente al handler que se encarga de to2
 client.on("message", commandHandler)
